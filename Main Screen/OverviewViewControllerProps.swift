7e32e53b5019dab230888369d56db2693d7bc33f
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2019-01-28 00:47:12 +0200

    Add some commands and intermediate states

--- Main Screen/OverviewViewControllerProps.swift
+++ Main Screen/OverviewViewControllerProps.swift
@@ -1,27 +1,49 @@
 extension OverviewViewController {
     struct Props {
-        let actions: [ActionElement]
-        let states: [StateElement]
-        let props: [PropsElement]
+        let actions: [Action]
+        let newAction: NewEntry
+        let states: [State]
+        let newState: NewEntry
         
-        struct ActionElement {
+        let props: [Props]
+        let newProps: NewEntry
+        
+        struct Action {
             let name: String
             let fields: [Field]
+            let connectOutput: Command?
         }
         
-        struct StateElement {
+        struct State {
             let name: String
             let fields: [Field]
+            let connectInput: Command?
+            let connectOutput: Command?
         }
         
-        struct PropsElement {
+        struct Props {
             let name: String
             let fields: [Field]
+            let connectInput: Command?
         }
         
         struct Field {
             let name: String
             let type: String
+        }
+        
+        struct NewEntry {
+            let activate: Command
+            let dismiss: Command
+            let name: String
+            let updateName: CommandWith<String>
+            let status: Status
+            
+            enum Status {
+                case empty
+                case valid(save: Command)
+                case invalid(reason: String)
+            }
         }
     }
 }
